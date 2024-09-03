65ee4a318f6177c3d4cea26919fbb1af712ab141
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2024-09-04 01:16:18 +0300

    login style

--- Signin And signup/index.js
+++ Signin And signup/index.js
@@ -4,6 +4,9 @@ const signin_signup = document.querySelector(".signin-signup");
 const page_two_btn = document.querySelector("#page_two_btn");
 const page_two_pre = document.querySelector("#page_two_pre");
 const page_three_pre = document.querySelector("#page_three_pre");
+const regester = document.querySelector("#regester");
+const container = document.querySelector(".container");
+const panels_container = document.querySelector(".panels-container");
 
 page_one_btn.addEventListener("click", () => {
   signin_signup.classList.add("done");
@@ -16,4 +19,10 @@ page_two_btn.addEventListener("click", () => {
 });
 page_three_pre.addEventListener("click", () => {
   signin_signup.classList.remove("end_done");
+});
+
+regester.addEventListener("click", () => {
+  container.classList.add("sign_up_mode");
+  panels_container.style.cssText = `left:-50%`;
+  signin_signup.style.cssText = `left:-473px`;
 });
