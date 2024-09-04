3588ca10534c48ef15bfb756746d80bc7a689a06
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2024-09-04 15:20:04 +0300

    rename forms num - 1

--- Signin And signup/index.js
+++ Signin And signup/index.js
@@ -26,5 +26,58 @@ regester.addEventListener("click", () => {
   panels_container.style.cssText = `left:-50%`;
   signin_signup.style.cssText = `left:-473px`;
 });
-
 // --------------------------------------------------
+// const email = document.querySelector("#email");
+// const password = document.querySelector("#password");
+// const emailError = document.querySelector("#email-error");
+// const passwordError = document.querySelector("#password-error");
+//   const emailRegex =
+//     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     if (!emailRegex.test(email.value) || !email.value) {
+//       emailError.textContent = "You must enter a valid email";
+//       emailError.style.display = "block";
+//     }
+
+//     if (password.value.length < 8) {
+//       passwordError.textContent = "Password must be at least 8 characters long";
+//       passwordError.style.display = "block";
+//     }
+// &&
+// emailRegex.test(email.value) &&
+// password.value.length >= 8
+// --------------------------------------------------
+// const form_1 = document.querySelector("#form-1");
+// form_1.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   const Fname = document.querySelector("#Fname");
+//   const Lname = document.querySelector("#Lname");
+//   const fnameError = document.querySelector("#fname-error");
+//   const lnameError = document.querySelector("#lname-error");
+
+//   fnameError.style.display = "none";
+//   lnameError.style.display = "none";
+//   emailError.style.display = "none";
+//   passwordError.style.display = "none";
+
+//   if (!Fname.value) {
+//     fnameError.textContent = "First name is required";
+//     fnameError.style.display = "block";
+//   }
+
+//   if (!Lname.value) {
+//     lnameError.textContent = "Last name is required";
+//     lnameError.style.display = "block";
+//   }
+
+//   if (Fname.value && Lname.value) {
+//     localStorage.setItem(
+//       "userData",
+//       JSON.stringify({
+//         firstName: Fname.value,
+//         lastName: Lname.value,
+//       })
+//     );
+//   }
+// });
+
+// -------------------------------------------------
